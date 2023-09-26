d4ca788f0b566f4bc49d3d4d12a9dd94ec3c741d
HoangQuan152000 <nguyenvanhoangquan15@gmail.com>
2023-09-26 17:16:33 +0700

    algorithm optimization

--- openCVtest1/main.cpp
+++ openCVtest1/main.cpp
@@ -1,4 +1,5 @@
-﻿#include <opencv2/core.hpp>
+﻿
+#include <opencv2/core.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
 #include <iostream>
@@ -6,15 +7,18 @@
 using namespace cv;
 using namespace std;
 
-void imp_dilate(Mat A, Size size, int shape);
-enum shape_kernel {
-    CROSS_XXX,
-    RECTANGLE_XXX
-};
+Mat dilate(Mat img, Mat struct_element);
+
 
 int main() {
-
-    // custom matrix A
+    // Đọc ảnh
+    Mat img = imread("sample.jpg");
+    if (img.empty()) {
+        cout << "khong doc duoc anh!";
+        return 0;
+    }
+    resize(img, img, Size(640, 460));
+    // Khai báo ma trận cấu trúc cross
     Mat A = (Mat_<uchar>(16, 16) <<
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -32,225 +36,70 @@ int main() {
         0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-    /*
- Mat A = imread("sample.jpg");
- Scalar();
- if (A.empty()) {
-     cout << "can not read image";
-     return 1;
- }
- resize(A, A, Size(640, 460));
- */
 
- // chọn loại ma trận
-    shape_kernel kernel_choice;
-    int input_shape;
-
-    // Lặp lại cho đến khi người dùng nhập vào một giá trị hợp lệ
-    while (1) {
-        // Hiển thị danh sách các loại kernel
-        cout << "Chon loai kernel: " << endl;
-        cout << "1. CROSS" << endl;
-        cout << "2. RECTANGLE" << endl;
-
-        // Đọc đầu vào của người dùng
-        cin >> input_shape;
-
-        // Kiểm tra xem đầu vào có hợp lệ hay không
-        if (input_shape == 1) {
-            kernel_choice = CROSS_XXX;
-            break;
-        }
-        else if (input_shape == 2) {
-            kernel_choice = RECTANGLE_XXX;
-            break;
-        }
-        else {
-            cout << "Nhap lai." << endl;
-        }
-    }
-    // create size object and input width , height
-
-    Size size;
-    double w;
-    double h;
-
-tryAgain:
-    cout << "nhap chieu rong kernel: ";
-    cin >> w;
-    size.width = (int)w;
-    cout << "nhap chieu cao kernel: ";
-    cin >> h;
-    size.height = (int)h;
-
-    if (size.width <= 0 || size.height <= 0) {
-        cout << "chieu rong va chieu cao phai la so duong!" << endl;
-        goto tryAgain;
-    }
-
-    imp_dilate(A, size, input_shape);
+    Mat struct_element = getStructuringElement(MORPH_CROSS, Size(3, 3));
 
 
+    // Thực hiện phép giãn nở
+    Mat result = dilate(img, struct_element);
 
+    // Hiển thị kết quả
+    imshow("Result", result);
     waitKey(0);
 
     return 0;
 }
+Mat dilate(Mat img, Mat struct_element) {
 
-
-void imp_dilate(Mat A, Size size, int shape) {
-
-    // create height and width for kernel
-    int h = size.height;
-    int w = size.width;
-
-    // check type data A
-    if (A.type() != CV_8UC1) {
-        cvtColor(A, A, COLOR_BGR2GRAY);
+    if (img.type() != CV_8UC1) {
+        cvtColor(img, img, COLOR_BGR2GRAY);
+        threshold(img, img, 128, 255, THRESH_BINARY);
     }
-    else threshold(A, A, 0, 255, THRESH_BINARY);
+    else  threshold(img, img, 0, 255, THRESH_BINARY);
 
 
-    Mat dst = Mat(A.rows, A.cols, CV_8UC1);
-    resize(dst, dst, Size(640, 460));
 
-    // browse each pixel
-    for (int i = 0; i < A.rows; i++) {
-        for (int j = 0; j < A.cols; j++) {
-            // Initialize the initial value for each pixel
-            dst.at<uchar>(i, j) = 0;
-            // matrix rectangle kernel
-            if (shape == 2) {
-                // scan with a non - square matrix
-                if (h != w) {
-                    for (int r = 0; r < h; r++) {
-                        for (int c = 0; c < w; c++) {
-                            // Check pixels are within matrix A
-                            if (i + r >= 0 && i + r < A.rows && j + c >= 0 && j + c < A.cols) {
-                                if (dst.at<uchar>(i, j) < A.at<uchar>(i + r, j + c)) {
-                                    dst.at<uchar>(i, j) = A.at<uchar>(i + r, j + c);
-                                }
-                            }
-                        }
-                    }
-                }
-                // scan with even matrix
-                else if (h % 2 == 0 && w % 2 == 0) {
-                    for (int r = -(h - 1) / 2; r <= h / 2; r++) {
-                        for (int c = -(w - 1); c <= w / 2; c++) {
-                            // Check pixels are within matrix A
-                            if (i + r >= 0 && i + r < A.rows && j + c >= 0 && j + c < A.cols) {
-                                if (dst.at<uchar>(i, j) < A.at<uchar>(i + r, j + c)) {
-                                    dst.at<uchar>(i, j) = A.at<uchar>(i + r, j + c);
-                                }
-                            }
-                        }
-                    }
-                }
-                // scan with odd matrix
-                else for (int r = -h / 2; r <= h / 2; r++) {
-                    for (int c = -w / 2; c <= w / 2; c++) {
+    // Kiểm tra kích thước ma trận cấu trúc
+    if (struct_element.cols == 0 || struct_element.rows == 0) {
+        return img;
+    }
 
-                        // Check pixels are within matrix A
-                        if (i + r >= 0 && i + r < A.rows && j + c >= 0 && j + c < A.cols) {
-                            if (dst.at<uchar>(i, j) < A.at<uchar>(i + r, j + c)) {
-                                dst.at<uchar>(i, j) = A.at<uchar>(i + r, j + c);
-                            }
-                        }
+
+    // Lấy kích thước ảnh
+    int h = img.rows;
+    int w = img.cols;
+
+    int x = struct_element.cols;
+    int y = struct_element.rows;
+
+    // Khởi tạo ma trận kết quả
+    Mat result = img.clone();
+
+    // Duyệt qua tất cả các pixel trong ảnh
+    for (int i = 0; i < h; i++) {
+        for (int j = 0; j < w; j++) {
+
+            for (int r = i - y / 2; r <= i + y / 2; r++) {
+                for (int c = j - x / 2; c <= j + x / 2; c++) {
+                    if (r >= 0 && r < h && c >= 0 && c < w && img.at<uchar>(r, c) == 255 &&
+                        (r - i + y / 2) < y && (c - j + x / 2) < x &&
+                        struct_element.at<uchar>((r - i + y / 2), (c - j + x / 2)) != 0)
+                    {
+                        result.at<uchar>(i, j) = 255;
+                        break;
                     }
                 }
             }
-            // matrix cross kernel
-            if (shape == 1) {
-                // matrix 1 x n or n x 1
-                if (h == 1 || w == 1) {
-                    if (h == 1) {
-                        for (int c = -(w - 1) / 2; c <= w / 2; c++) {
-                            if (j + c >= 0 && j + c < A.cols) {
-                                if (dst.at<uchar>(i, j) < A.at<uchar>(i, j + c)) {
-                                    dst.at<uchar>(i, j) = A.at<uchar>(i, j + c);
-                                }
-                            }
-                        }
-                    }
-                    else for (int r = -(h - 1) / 2; r <= h / 2; r++) {
-                        if (i + r >= 0 && i + r < A.rows) {
-                            if (dst.at<uchar>(i, j) < A.at<uchar>(i + r, j)) {
-                                dst.at<uchar>(i, j) = A.at<uchar>(i + r, j);
-                            }
-                        }
-                    }
-                } // 1 or 2 dimension is even
-                else if (h % 2 == 0 || w % 2 == 0) {
-                    // h even ,  w odd
-                    if (h % 2 == 0 && w % 2 != 0) {
-                        for (int r = -(h + 1) / 2; r < h / 2; r++) {
-                            for (int c = -w / 2; c <= w / 2; c++) {
-                                if (i + r >= 0 && i + r < A.rows && j + c >= 0 && j + c < A.cols) {
-                                    // Find max in each array 1 x n and n x 1
-                                    if (dst.at<uchar>(i, j) < A.at<uchar>(i + r, j)) {
-                                        dst.at<uchar>(i, j) = A.at<uchar>(i + r, j);
-                                    }
-                                    if (dst.at<uchar>(i, j) < A.at<uchar>(i, j + c)) {
-                                        dst.at<uchar>(i, j) = A.at<uchar>(i, j + c);
-                                    }
-                                }
-                            }
-
-                        }
-                    }
-                    // h odd , w even
-                    else if (h % 2 != 0 && w % 2 == 0) {
-                        for (int c = -(w + 1) / 2; c < w / 2; c++) {
-                            for (int r = -h / 2; r <= h / 2; r++) {
-                                if (i + r >= 0 && i + r < A.rows && j + c >= 0 && j + c < A.cols) {
-                                    // Find max in each array 1 x n and n x 1
-                                    if (dst.at<uchar>(i, j) < A.at<uchar>(i + r, j)) {
-                                        dst.at<uchar>(i, j) = A.at<uchar>(i + r, j);
-                                    }
-                                    if (dst.at<uchar>(i, j) < A.at<uchar>(i, j + c)) {
-                                        dst.at<uchar>(i, j) = A.at<uchar>(i, j + c);
-                                    }
-                                }
-                            }
-
-                        }
-                    }
-                    // h,w is even
-                    else for (int c = -(w + 1) / 2; c < w / 2; c++) {
-                        for (int r = -(h + 1) / 2; r < h / 2; r++) {
-                            if (i + r >= 0 && i + r < A.rows && j + c >= 0 && j + c < A.cols) {
-                                // Find max in each array 1 x n and n x 1
-                                if (dst.at<uchar>(i, j) < A.at<uchar>(i + r, j)) {
-                                    dst.at<uchar>(i, j) = A.at<uchar>(i + r, j);
-                                }
-                                if (dst.at<uchar>(i, j) < A.at<uchar>(i, j + c)) {
-                                    dst.at<uchar>(i, j) = A.at<uchar>(i, j + c);
-                                }
-                            }
-                        }
-
-                    }
-                }
-                // h,w is odd
-                else for (int c = -w / 2; c <= w / 2; c++) {
-                    for (int r = -h / 2; r <= h / 2; r++) {
-                        if (i + r >= 0 && i + r < A.rows && j + c >= 0 && j + c < A.cols) {
-                            // Find max in each array 1 x n and n x 1
-                            if (dst.at<uchar>(i, j) < A.at<uchar>(i + r, j)) {
-                                dst.at<uchar>(i, j) = A.at<uchar>(i + r, j);
-                            }
-                            if (dst.at<uchar>(i, j) < A.at<uchar>(i, j + c)) {
-                                dst.at<uchar>(i, j) = A.at<uchar>(i, j + c);
-                            }
-                        }
-                    }
-
-                }
-            }
-
 
         }
     }
-    imshow("dst", dst);
+
+    return result;
 }
+
+
+
+
+
+
+
