2fe1dbe8b009b134bec28c08305d1e8808d79b58
HoangQuan152000 <nguyenvanhoangquan15@gmail.com>
2023-09-27 09:20:38 +0700

    add comment

--- openCVtest1/main.cpp
+++ openCVtest1/main.cpp
@@ -7,18 +7,19 @@
 using namespace cv;
 using namespace std;
 
-Mat dilate(Mat img, Mat struct_element);
+Mat dilate_im(Mat img, Mat struct_element);
 
 
 int main() {
-    // Đọc ảnh
+    // load image from file
     Mat img = imread("sample.jpg");
     if (img.empty()) {
         cout << "khong doc duoc anh!";
         return 0;
     }
     resize(img, img, Size(640, 460));
-    // Khai báo ma trận cấu trúc cross
+
+    // initialize the source matrix
     Mat A = (Mat_<uchar>(16, 16) <<
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -37,56 +38,63 @@ int main() {
         0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
-    Mat struct_element = getStructuringElement(MORPH_CROSS, Size(3, 3));
+    // initialize the struct element matrix
+    Mat struct_element = getStructuringElement(MORPH_CROSS, Size(1, 3));
 
 
-    // Thực hiện phép giãn nở
-    Mat result = dilate(img, struct_element);
+    // call dilate function 
+    Mat result = dilate_im(img, struct_element);
 
-    // Hiển thị kết quả
+
     imshow("Result", result);
     waitKey(0);
 
     return 0;
 }
-Mat dilate(Mat img, Mat struct_element) {
+Mat dilate_im(Mat img, Mat struct_element) {
 
+
+    // check type data , convert to gray image 
     if (img.type() != CV_8UC1) {
         cvtColor(img, img, COLOR_BGR2GRAY);
-        threshold(img, img, 128, 255, THRESH_BINARY);
     }
-    else  threshold(img, img, 0, 255, THRESH_BINARY);
 
 
-
-    // Kiểm tra kích thước ma trận cấu trúc
+    // check size kernel 
     if (struct_element.cols == 0 || struct_element.rows == 0) {
         return img;
     }
 
 
-    // Lấy kích thước ảnh
+    // inital variable size image , kernel
     int h = img.rows;
     int w = img.cols;
 
     int x = struct_element.cols;
     int y = struct_element.rows;
 
-    // Khởi tạo ma trận kết quả
+    // inital img dst
     Mat result = img.clone();
 
-    // Duyệt qua tất cả các pixel trong ảnh
+    // scan each pixel 
     for (int i = 0; i < h; i++) {
         for (int j = 0; j < w; j++) {
 
+            // scan each pixel within kernel 
             for (int r = i - y / 2; r <= i + y / 2; r++) {
                 for (int c = j - x / 2; c <= j + x / 2; c++) {
-                    if (r >= 0 && r < h && c >= 0 && c < w && img.at<uchar>(r, c) == 255 &&
+
+                    // check access out of range size image 
+                    if (r >= 0 && r < h && c >= 0 && c < w &&
+                        //check access out of range size kernel
                         (r - i + y / 2) < y && (c - j + x / 2) < x &&
+                        // remove kernel 0 value position
                         struct_element.at<uchar>((r - i + y / 2), (c - j + x / 2)) != 0)
                     {
-                        result.at<uchar>(i, j) = 255;
-                        break;
+                        // Find the maximum value of neighboring pixels
+                        if (result.at<uchar>(i, j) < img.at<uchar>(r, c)) {
+                            result.at<uchar>(i, j) = img.at<uchar>(r, c);
+                        }
                     }
                 }
             }
